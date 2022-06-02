31bfcd8f67fa26d74f3d94e46f31070975605d6e
Adegoke Ismail <adegokeismail77.com>
2022-06-02 22:26:07 +0100

    getting started with c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print text to standard error
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19\n");
+	return (1);
+}
